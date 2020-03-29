b708328d0a1d91e8b785770eace98b96b45252e2
JrBour <calaruni@hotmail.com>
2020-03-29 17:29:09 +0200

    feat(input): Add input component

---
+++ components/input/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input :type="type" :value="value" @change="onChange" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    onChange: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
