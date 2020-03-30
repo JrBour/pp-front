c0006a60df5b842de4f6c7e4d04b7aa5f4accf3d
JrBour <calaruni@hotmail.com>
2020-03-30 21:06:15 +0200

    feat(input): Add label in input component

--- components/input/index.vue
+++ components/input/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
+    <label>{{ text }}</label>
     <input :type="type" :value="value" @change="onChange" />
   </div>
 </template>
 <script>
 export default {
   props: {
+    text: {
+      type: String,
+      required: true
+    },
     value: {
       type: String,
       required: true
