0abb0f0517b7f25d598a8c5f59792eb954e8ff54
JrBour <calaruni@hotmail.com>
2020-06-02 17:50:26 +0200

    feat(google): Submit google form from login page and retrieve token

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -40,6 +40,7 @@ export default {
   data: () => ({
     email: '',
     password: '',
+    clientId: process.env.NUXT_ENV_GOOGLE_CLIENT_ID,
     errors: {
       email: '',
       password: '',
@@ -63,12 +64,47 @@ export default {
       return false
     }
   },
+  beforeMount() {
+    this.oauthSignIn()
+  },
   beforeCreate() {
     if (Cookies.get('token') !== '') {
       this.$router.push('events')
     }
   },
   methods: {
+    oauthSignIn() {
+      // Google's OAuth 2.0 endpoint for requesting an access token
+      const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth'
+
+      // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+      const form = document.createElement('form')
+      form.setAttribute('method', 'GET') // Send as a GET request.
+      form.setAttribute('action', oauth2Endpoint)
+
+      // Parameters to pass to OAuth 2.0 endpoint.
+      const params = {
+        client_id: this.clientId,
+        redirect_uri: 'https://pp.jeremybourel.fr/login',
+        response_type: 'token',
+        scope: 'https://www.googleapis.com/auth/drive.metadata.readonly',
+        include_granted_scopes: 'true',
+        state: 'pass-through value'
+      }
+
+      // Add form parameters as hidden input values.
+      for (const p in params) {
+        const input = document.createElement('input')
+        input.setAttribute('type', 'hidden')
+        input.setAttribute('name', p)
+        input.setAttribute('value', params[p])
+        form.appendChild(input)
+      }
+
+      // Add form to page and submit it to open the OAuth 2.0 endpoint.
+      document.body.appendChild(form)
+      form.submit()
+    },
     handleChangeField(name, value) {
       this[name] = value
       this.errors[name] = validateLoginField(name, value)
