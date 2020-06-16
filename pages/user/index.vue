80f5c7431ed040696ef3d2548a18b3936089f2e8
JrBour <calaruni@hotmail.com>
2020-06-16 20:12:49 +0200

    feat(user): Add button for send friend request

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,10 +1,17 @@
 <template>
-  <h1>{{ user.name }}</h1>
+  <div>
+    <h1>{{ user.name }}</h1>
+    <Button text="Envoyer une demande d'ami" />
+  </div>
 </template>
 <script>
 import axiosHelper from '~/lib/axiosHelper'
+import Button from '~/components/button'
 
 export default {
+  components: {
+    Button
+  },
   data: () => ({
     user: null
   }),
