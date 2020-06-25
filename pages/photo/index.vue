b67a2b8035fa1c87aebd671a990abfecfed3bf7f
JrBour <calaruni@hotmail.com>
2020-06-25 22:34:14 +0200

    refactor(photo): Add loader if the picture is not load

---
+++ pages/photo/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img v-if="image !== null" :src="image" alt="" />
+    <Loader v-else />
+  </div>
+</template>
+<script>
+import Loader from '~/components/loader'
+
+export default {
+  components: {
+    Loader
+  },
+  data: () => ({
+    image: null
+  })
+}
+</script>
