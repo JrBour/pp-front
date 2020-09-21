cbfc3b66571d5fe660643b262558105f9799322a
JrBour <calaruni@hotmail.com>
2020-09-21 20:51:28 +0200

    feat(vee-validate): Add dependency and create plugin for integrate vee-validate with Nuxt

---
+++ plugins/vee-validate.js
@@ -0,0 +1,30 @@
+import { extend } from 'vee-validate'
+import { required, email, max, min } from 'vee-validate/dist/rules'
+
+extend('required', {
+  ...required,
+  message: 'Ce champ est requis'
+})
+
+extend('min', {
+  ...min,
+  validate(value, args) {
+    return value.length >= args.length
+  },
+  params: ['length'],
+  message: 'Ce champs doit contenir au moins {length} caracteres'
+})
+
+extend('max', {
+  ...max,
+  validate(value, args) {
+    return value.length <= args.length
+  },
+  params: ['length'],
+  message: 'Ce champ doit contenir au maximum {length} caracteres'
+})
+
+extend('email', {
+  ...email,
+  message: 'Email invalide'
+})
