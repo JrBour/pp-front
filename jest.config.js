93480717cd3d337f45af5d073dfc9a83d6a08e77
JBourel <calaruni@hotmail.com>
2021-05-31 19:11:00 +0200

    fix(eslint): Upgrade eslint-config and eslint-module for fix eslint issue with nuxt

--- jest.config.js
+++ jest.config.js
@@ -7,18 +7,18 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/$1'
   },
   transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|png|svg|jpg|ttf|woff|woff2)$':
       'jest-transform-stub',
-    'vee-validate/dist/rules': '<rootDir>/node_modules/babel-jest'
+    'vee-validate/dist/rules': 'babel-jest'
   },
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
     '<rootDir>/test/',
     '<rootDir>/components/__tests__/setup.js'
   ],
-  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+  snapshotSerializers: ['jest-serializer-vue'],
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
