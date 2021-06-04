8413b54ff485b1454c0cbb3444c95171b9b5efff
JBourel <calaruni@hotmail.com>
2021-06-04 18:53:19 +0200

    test(baseInput): Add test for check if watchers works correctly

--- components/__tests__/BaseInput/index.test.js
+++ components/__tests__/BaseInput/index.test.js
@@ -4,13 +4,13 @@ import BaseInput from '@/components/BaseInput'
 
 describe('BaseInput', () => {
   let wrapper
-
+  const mockOnChange = jest.fn();
   beforeEach(() => {
     wrapper = shallowMount(BaseInput, {
       propsData: {
         text: 'Field',
         name: 'name',
-        onChange: jest.fn(),
+        onChange: mockOnChange,
         defaultValue: 'test',
         error: ''
       },
@@ -18,6 +18,21 @@ describe('BaseInput', () => {
         Loader: true,
         ValidationProvider
       }
+    })
+  })
+
+  describe('watcher', () => {
+    it('should call onChange method when inputValue is edit', async () => {
+      await wrapper.setData({
+        inputValue: 'Test'
+      })
+      expect(mockOnChange.mock.calls.length).toBe(1)
+    })
+    it('should edit inputValue value when defaultValue is edit', async () => {
+      await wrapper.setProps({
+        defaultValue: 'testtttt'
+      })
+      expect(wrapper.vm.inputValue).toBe('testtttt')
     })
   })
 
