471042562fd144d18e8a0718fcc2590a457b8e5b
JBourel <calaruni@hotmail.com>
2021-06-23 18:53:06 +0200

    test(Expense): Add test to check if amount computed method works

--- components/__tests__/Expense/index.test.js
+++ components/__tests__/Expense/index.test.js
@@ -1,13 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
 import Expense from '@/components/Expense'
 
-describe('Expense', () => {
-  let wrapper
-
-  beforeEach(() => {
-    wrapper = shallowMount(Expense, {
-      propsData: {
-        expense: {
+const expenseMock = {
           name: 'Name',
           user: {
             givenName: 'John',
@@ -15,7 +9,15 @@ describe('Expense', () => {
           },
           amount: 12.12,
           createdAt: '12/03/2021'
-        },
+        }
+
+describe('Expense', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallowMount(Expense, {
+      propsData: {
+        expense: expenseMock,
         canRemoveExpense: true
       }
     })
@@ -26,6 +28,27 @@ describe('Expense', () => {
       const button = wrapper.find('button');
       await button.trigger('click');
       expect(wrapper.emitted('remove-expense')).toBeTruthy()
+    })
+  })
+
+  describe('computed', () => {
+    it('should change amount computed method according amount props', async () => {
+      expect(wrapper.vm.amount).toBe(12.12)
+      await wrapper.setProps({
+        expense: {
+          ...expenseMock,
+          amount: 12
+        }
+      })
+      expect(wrapper.vm.amount).toBe("12.00")
+
+      await wrapper.setProps({
+        expense: {
+          ...expenseMock,
+          amount: 12.1
+        }
+      })
+      expect(wrapper.vm.amount).toBe("12.10")
     })
   })
 
