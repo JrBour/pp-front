ef5a2e5d9a73b63617e5f3ad0e9eadc31c37114b
JBourel <calaruni@hotmail.com>
2021-06-26 19:14:15 +0200

    test(Notification): Check if submit-response method is emit when we click on button

--- components/__tests__/Notification/index.test.js
+++ components/__tests__/Notification/index.test.js
@@ -4,7 +4,7 @@ import Notification from '@/components/Notification'
 const event = {
   id: '12',
   startAt: '12/03/2021',
-  name: 'Event name',
+  name: 'Event name Lorem ipsum Lorem ipsum',
   address: '12 rue du gnou',
   city: 'Paris'
 }
@@ -17,6 +17,14 @@ describe('Notification', () => {
       propsData: {
         event
       }
+    })
+  })
+
+  describe('method', () => {
+    it('should emit submit-response method', async () => {
+      const acceptButton = wrapper.findAll('button').at(0)
+      await acceptButton.trigger('click')
+      expect(wrapper.emitted('submit-response')).toBeTruthy()
     })
   })
 
